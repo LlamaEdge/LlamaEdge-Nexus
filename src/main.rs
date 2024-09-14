7405ac7671acfaf26ec9566307d71bb6e1f33cda
Xin Liu <sam@secondstate.io>
2024-09-14 16:28:02 +0900

    first commit
    Signed-off-by: Xin Liu <sam@secondstate.io>

---
+++ src/main.rs
@@ -0,0 +1,103 @@
+#[macro_use]
+extern crate log;
+
+use axum::{
+    body::{Body, Bytes},
+    extract::{Path, State},
+    http::{Request, Response, StatusCode, Uri},
+    routing::post,
+    Router,
+};
+use hyper::{client::HttpConnector, Client};
+use serde_json::Value;
+use std::{net::SocketAddr, sync::Arc};
+use tokio::net::TcpListener;
+
+type SharedClient = Arc<Client<HttpConnector>>;
+
+#[derive(Clone)]
+struct AppState {
+    client: SharedClient,
+    chat_url: Uri,
+    image_url: Uri,
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() {
+    // Create a shared HTTP client
+    let client = Arc::new(Client::new());
+
+    let app_state = AppState {
+        client,
+        chat_url: "http://locahost:12345".parse().unwrap(),
+        image_url: "http://localhost:12306".parse().unwrap(),
+    };
+
+    // Build our application with routes
+    let app = Router::new()
+        .route("/v1/chat/completions", post(chat_handler))
+        .route("/v1/image/generation", post(image_handler))
+        .with_state(app_state);
+
+    // Run it
+    let addr = "0.0.0.0:12123";
+    let tcp_listener = TcpListener::bind(addr).await.unwrap();
+    println!("LlamaEdge listening on {}", addr);
+    if let Err(e) = axum::Server::from_tcp(tcp_listener.into_std().unwrap())
+        .unwrap()
+        .serve(app.into_make_service())
+        .await
+    {
+        println!("server error: {}", e);
+    }
+}
+
+async fn chat_handler(
+    State(state): State<AppState>,
+    req: Request<Body>,
+) -> Result<Response<Body>, StatusCode> {
+    println!("In chat_handler");
+
+    proxy_request(state.client, req, state.chat_url).await
+}
+
+async fn image_handler(
+    State(state): State<AppState>,
+    req: Request<Body>,
+) -> Result<Response<Body>, StatusCode> {
+    println!("In image_handler");
+
+    proxy_request(state.client, req, state.image_url).await
+}
+
+async fn proxy_request(
+    client: SharedClient,
+    mut req: Request<Body>,
+    downstream_url: Uri,
+) -> Result<Response<Body>, StatusCode> {
+    // Change the request URL to the downstream server
+    let path_and_query = req
+        .uri()
+        .path_and_query()
+        .map(|x| x.to_string())
+        .unwrap_or_default();
+    println!("path_and_query: {}", path_and_query);
+
+    let mut downstream_url_s = downstream_url.to_string();
+    downstream_url_s = downstream_url_s.trim_end_matches('/').to_string();
+    println!("downstream_url_s: {}", downstream_url_s);
+
+    let new_uri = format!("{}{}", downstream_url_s, path_and_query)
+        .parse()
+        .unwrap();
+
+    println!("new_uri: {}", new_uri);
+
+    *req.uri_mut() = new_uri;
+
+    // Forward the request to the downstream server
+    match client.request(req).await {
+        Ok(res) => Ok(res),
+        Err(_) => Err(StatusCode::BAD_GATEWAY),
+    }
+}

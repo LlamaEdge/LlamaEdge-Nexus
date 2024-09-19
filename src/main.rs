0dd573dcdd9db7e0e3eb2c6668ebb7bb1ba6c8c0
Xin Liu <sam@secondstate.io>
2024-09-19 15:59:42 +0900

    feat: add `--port` CLI option
    Signed-off-by: Xin Liu <sam@secondstate.io>

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 #[macro_use]
 extern crate log;
 
+mod utils;
+
+use anyhow::Result;
 use async_trait::async_trait;
 use axum::extract::Extension;
 use axum::{
@@ -10,21 +13,27 @@ use axum::{
     routing::post,
     Router,
 };
+use clap::Parser;
 use hyper::{client::HttpConnector, Client};
 use serde_json::Value;
-use std::fmt::{self, write};
+use std::fmt;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::RwLock;
 use std::{net::SocketAddr, sync::Arc};
 use tokio::net::TcpListener;
+use utils::LogLevel;
 
 type SharedClient = Arc<Client<HttpConnector>>;
+
+// default port of LlamaEdge Gateway
+const DEFAULT_PORT: &str = "8080";
 
 #[async_trait]
 pub trait RoutingPolicy {
     fn next(&self) -> Uri;
 }
 
+/// Represents a LlamaEdge API server
 #[derive(Debug)]
 struct Server {
     url: Uri,
@@ -123,8 +132,36 @@ impl fmt::Display for UrlType {
     }
 }
 
+#[derive(Debug, Parser)]
+#[command(name = "LlamaEdge Gateway", version = env!("CARGO_PKG_VERSION"), author = env!("CARGO_PKG_AUTHORS"), about = "LlamaEdge Gateway")]
+struct Cli {
+    /// Socket address of LlamaEdge API Server instance
+    #[arg(long, default_value = DEFAULT_PORT, value_parser = clap::value_parser!(u16))]
+    port: u16,
+}
+
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
+    // get the environment variable `RUST_LOG`
+    let rust_log = std::env::var("RUST_LOG").unwrap_or_default().to_lowercase();
+    let (_, log_level) = match rust_log.is_empty() {
+        true => ("stdout", LogLevel::Info),
+        false => match rust_log.split_once("=") {
+            Some((target, level)) => (target, level.parse().unwrap_or(LogLevel::Info)),
+            None => ("stdout", rust_log.parse().unwrap_or(LogLevel::Info)),
+        },
+    };
+
+    // set global logger
+    wasi_logger::Logger::install().expect("failed to install wasi_logger::Logger");
+    log::set_max_level(log_level.into());
+
+    // parse the command line arguments
+    let cli = Cli::parse();
+
+    // log the version of the server
+    info!(target: "stdout", "version: {}", env!("CARGO_PKG_VERSION"));
+
     // Create a shared HTTP client
     let client = Arc::new(Client::new());
 
@@ -139,9 +176,10 @@ async fn main() {
         .with_state(app_state);
 
     // Run it
-    let addr = "0.0.0.0:12123";
-    let tcp_listener = TcpListener::bind(addr).await.unwrap();
-    println!("LlamaEdge listening on {}", addr);
+    let addr = format!("127.0.0.1:{}", cli.port);
+    let tcp_listener = TcpListener::bind(&addr).await.unwrap();
+    info!(target: "stdout", "Listening on {}", addr);
+
     if let Err(e) = axum::Server::from_tcp(tcp_listener.into_std().unwrap())
         .unwrap()
         .serve(app.into_make_service())
